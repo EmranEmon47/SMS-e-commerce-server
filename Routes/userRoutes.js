974fa981da877fae068cdec396bbbc7536964ae1
firasel <mdrasel601390@gmail.com>
2022-07-14 13:03:21 +0600

    Signin & Signup api created

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const Signup = require("../Controllers/user/signup");
 const Signin = require("../Controllers/user/signin");
-// const VerifyToken = require("../Controllers/User/VerifyToken");
 const router = express.Router();
 
 // Signup
 router.post("/signup", Signup);
 // Signin
 router.post("/signin", Signin);
-// Verify user signin
-// router.get("/verify", VerifyToken);
 
 module.exports = router;
